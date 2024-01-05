3c101e35df92b69b996de20920cc6b453ef515f6
Rafi Rustian~ <rsrustian@gmail.com>
2024-01-06 05:45:26 +0700

    ngerename

--- Chapter03/Rafi_714232005/findelement.py
+++ Chapter03/Rafi_714232005/findelement.py
@@ -1,5 +1,5 @@
 from selenium.webdriver import Chrome
 
 driver = Chrome()
-driver.get("https://sso-siasn.bkn.go.id/auth/realms/public-siasn/protocol/openid-connect/auth?client_id=si-kinerja&redirect_uri=https%3A%2F%2Fkinerja.bkn.go.id%2Flogin&state=49535869-1f0b-47e0-9889-adb2ae2ef505&response_mode=fragment&response_type=code&scope=openid&nonce=b9a96742-0e8e-407e-92da-04233eaedd4e")
-driver.find_element_by_id("username").sendkeys("199004082020122007")
+driver.get("https://www.google.co.id/")
+driver.find_element("name", "q")
--- Chapter03/Rafi_714232005/test3.py
+++ Chapter03/Rafi_714232005/test3.py
@@ -3,7 +3,9 @@ from selenium.webdriver import Chrome
 driver1 = Chrome()
 driver2 = Chrome()
 driver3 = Chrome()
+driver4 = Chrome()
 
 driver1.get("https://www.google.com/?hl-ID")
 driver2.get("https://github.com/rplulbi/sqa")
-driver3.get("https://github.com/rplulbi/SQA/tree/main/Chapter3")
+driver3.get("https://github.com/rafirustian/SQA23-Rafi/tree/main/Chapter03/Rafi_714232005")
+driver4.get("https://putr.cianjurkab.go.id/administrator-login")
